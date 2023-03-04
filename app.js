357f59ad7306dd5b86361dc0f988984746eca0fd
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-03 21:06:26 -0500

    Parent School Class Fully Created

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 class School {
-  constructor()
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this.level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(newNumberOfStudents) {
+    if (typeof newNumberOfStudents === 'number') {
+      this._numberOfStudents = newNumberOfStudents;
+    } else {
+      console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+  }
+
+  quickFacts() {
+    console.log(
+      `${name} educates ${numberOfStudents} students at the ${level} school level.`
+    );
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    const randInt = Math.floor(substituteTeachers.length * Math.random());
+    return substituteTeachers[randInt];
+  }
 }
