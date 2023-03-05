1a745458a04eaf38ce1bda3ecb0056a80dd8db94
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-05 08:43:39 -0500

    School Catalogue Project with JS Classes Done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+'use strict';
 class School {
   constructor(name, level, numberOfStudents) {
     this._name = name;
     this._level = level;
-    this._numberOfStudents;
+    this._numberOfStudents = numberOfStudents;
   }
 
   get name() {
@@ -27,7 +28,7 @@ class School {
 
   quickFacts() {
     console.log(
-      `${name} educates ${numberOfStudents} students at the ${level} school level.`
+      `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`
     );
   }
 
@@ -39,10 +40,7 @@ class School {
 
 class PrimarySchool extends School {
   constructor(name, numberOfStudents, pickupPolicy) {
-    super(name);
-    this._level = primary;
-    this._name = name;
-    this._numberOfStudents = numberOfStudents;
+    super(name, 'primary', numberOfStudents);
     this._pickupPolicy = pickupPolicy;
   }
 
@@ -50,3 +48,40 @@ class PrimarySchool extends School {
     return this._pickupPolicy;
   }
 }
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool(
+  'Lorraine Hansbury',
+  514,
+  'Students must be picked up by a parent, guardian, or a family member over the age of 13.'
+);
+
+lorraineHansbury.quickFacts();
+
+School.pickSubstituteTeacher([
+  'Jamal Crawford',
+  'Lou Williams',
+  'J.R. Smith',
+  'James Harden',
+  'Jason Terry',
+  'Manu Ginobili',
+]);
+
+const alSmith = new HighSchool('Al E. Smith', 415, [
+  'Baseball',
+  'Basketball',
+  'Volleyball',
+  'Track and Field',
+]);
+
+console.log(alSmith.sportsTeams);
